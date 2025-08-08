a92a71d1ada35bddab5569269c5ad728c32faecf
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-08-08 11:34:30 +0200

    update

---
+++ Javascript/T3. Datos Number y String/script.js
@@ -0,0 +1,16 @@
+// NÚMEROS -> pueden ser con o sin punto (.)
+
+let variableNumero = 10 // Valor numérico
+let variableDecimal = 10.10 // Valor flotante 
+
+
+// TEXTO
+
+let variableTexto = 'Texto' // Valor String (cadena de caracteres)
+let char = 'b' // JavaScript también lo considera String
+let variableTexto2 = "Texto" // Valor String (cadena de caracteres)
+let variableTexto3 = `Esta es una variable distinta ${variableNumero}` 
+      // Traerá el valor de la variable junto con el texto.
+      // *** `Valor literal {expresión de JavaScript}`
+
+// Ejemplo de salida: "Esta es una variable distinta 10"
