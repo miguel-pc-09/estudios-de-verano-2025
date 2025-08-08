12dbf170e12ba9e3a80170faaf9033071f514785
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-08-08 15:58:23 +0200

    update

---
+++ Javascript/T4. Operadores en Javascript/script.js
@@ -0,0 +1,43 @@
+// Operadores
+
+const variable = 'Valor de la variable'
+
+// El " = " es el operador de ASIGNACION 
+
+// ARITMETICOS 
+ 
+ const suma = 1 +3             // + Es operador aritmetico de la SUMA
+ const resta = 1 - 3           // - Es operador aritmetico de la RESTA
+ const multiplicacion = 1 * 3  // * Es operador aritmetico de la MULTIPLICACION
+ const division = 1 / 3        // / Es operador aritmetico de la DIVISION
+ const resto = 5 % 2           // % Es el operador aritmetico que nos dara el resto de la division
+ const exponente = 3** 3      // ** Es el operador aritmetico de la exponencia -> en este caso es 3 elvado a 3
+
+ // Ejemplos con aumentadores y disminuidores ++ -- 
+ // Comprobar con liveserver apretando F12 o click derecho inspeccionar
+
+ let x = 5;
+ let y = 5;
+ x++; // Aumentador
+ // y++; // Estos dos ampliarian uno mas a cada uno de los dos.
+ // x--;
+ y--; //disminuye
+
+ // Utilizar console.log para ver los resultados en la consola de la web
+
+ console.log(variable)
+ console.log(suma)
+ console.log(resta)
+ console.log(multiplicacion)
+ console.log(division)
+ console.log(resto)
+ console.log(exponente)
+ console.log(x)
+ console.log(y)
+
+ /*
+    En la consola de la web igualmente podemos escribir por ejemplo
+    let suma2 = 5 + 6
+    console.log(suma2)
+    -> mostrara el resultado de 11
+ */
