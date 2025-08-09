8420464ebd8ba34e09459866b364f139b09c33d9
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-08-09 13:47:18 +0200

    update

---
+++ Javascript/T9. Consola Node.JS/script.js
@@ -0,0 +1,41 @@
+// Consola NODE.JS
+
+// Otra forma de ejecutar JavaScript sería con NODE.JS
+
+// Anteriormente hacíamos lo siguiente:
+
+// En Visual Studio Code nos metemos en script.js y escribimos la siguiente frase:
+
+let x = 'Este texto va a salir en consola'
+console.log(x)
+
+// Ahora, en index.html, mediante el enlace que ya teníamos o creamos al JS, 
+// podríamos abrir Chrome y ver los resultados en la consola.
+// En la consola de Chrome podremos ver lo anterior escrito: "Este texto va a salir en consola"
+
+// ---------------------------------------------
+
+// Ahora, la forma de ejecutar esto sin usar la consola del navegador 
+// es utilizando NODE.JS
+
+// En Visual Studio Code, estando en Windows, pulsamos Ctrl + ñ para abrir el terminal.
+// En Mac sería Ctrl + ` (tecla junto al número 1).
+// Dentro del terminal escribiremos lo siguiente:
+
+// node script.js
+
+// Al darle a ENTER nos saldrá el mensaje: "Este texto va a salir en consola"
+
+// ---------------------------------------------
+
+// Ahora cambiaremos el contenido de script.js por lo siguiente:
+
+let a = 5
+let b = 6
+
+console.log(a + b) // Esto debería darnos un 11 en consola
+
+/* 
+*** TODO ESTO SE HACE PORQUE ES LA FORMA MÁS HABITUAL 
+    EN BASES DE DATOS O EN EL CICLO DAW 
+*/
