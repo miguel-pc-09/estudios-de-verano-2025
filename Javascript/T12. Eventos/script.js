1d58d26c8a310b4769ec7fba1bf4d83cb3fc3730
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-08-10 10:53:04 +0200

    update

---
+++ Javascript/T12. Eventos/script.js
@@ -0,0 +1,6 @@
+function mostrar() {
+    document.getElementById('seleccionable').innerHTML = Date();
+    // document -> Lee toda la página.
+    // getElementById -> Selecciona el elemento con el id indicado ('seleccionable').
+}
+
