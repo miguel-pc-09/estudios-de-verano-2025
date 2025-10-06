5fa440712791997d2e2f671b170e23a220ee4ad3
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-10-06 16:01:50 +0200

    update

--- Python/Temario/10_functions.py
+++ Python/Temario/10_functions.py
@@ -1,9 +1,11 @@
 ## Functions ## 
 
 # Definición: Con las funciones intentamos resolver dos problemas:
-#        1º Intentamos encapsular una lógica muy concreta, es decir una función va a intentar resolver un problema muy concreto que nosotros vamos a indicar 
-#        2º Gracias a las funciones, vamos a evitar errores, porque, todo el código que va a resolver un problema muy concreto, va a estar en un único lugar, entonces
-#               siempre que querramos resolver ese problema, llamaremos a esa misma función.
+#        1º Intentamos encapsular una lógica muy concreta; es decir, una función va a intentar resolver
+#           un problema muy concreto que nosotros vamos a indicar.
+#        2º Gracias a las funciones, vamos a evitar errores, porque todo el código que va a resolver
+#           un problema muy concreto va a estar en un único lugar; entonces, siempre que queramos
+#           resolver ese problema, llamaremos a esa misma función.
 
 # Palabra reservada DEF + nombreFuncion():
 def my_function():
@@ -13,14 +15,16 @@ my_function()
 my_function()
 my_function()
 
-# Función con parámetros de entrada/argumentos: Se va a encargar de sumar dos valores. Y estos valores quiero indicarselo. 
+# Función con parámetros de entrada/argumentos: Se va a encargar de sumar dos valores.
+# Y estos valores quiero indicárselos.
 print("Función con parámetros de entrada/argumentos")
 def sum_two_values(first_value, second_value):
     print(first_value + second_value)
 
 sum_two_values(5, 7)
 sum_two_values(54754, 71231)
-sum_two_values("5", "7")        # Al ser cadena de texto lo concatena, si queremos que solo coja numeros, podriamos crear mediante type que sepa si es un int o no.
+sum_two_values("5", "7")        # Al ser cadena de texto, lo concatena; si queremos que solo coja números,
+                                # podríamos crear, mediante type, que sepa si es un int o no.
 sum_two_values(1.4, 5.2)
 print("")
 
@@ -31,11 +35,11 @@ def sum_two_values_with_return(first_value, second_value):
     return  first_value + second_value
 
 my_result = sum_two_values_with_return(10, 5)
-print(my_result)                               # podemos guardarlo en una variable y luego imprimir, o directamente imprimir
+print(my_result)                               # Podemos guardarlo en una variable y luego imprimir, o directamente imprimir.
 print(sum_two_values_with_return(10, 5))
 
-# con mas lineas dentro de la funcion
-print("Segunda forma creando mas lineas dentro de la función")
+# Con más líneas dentro de la función
+print("Segunda forma creando más líneas dentro de la función")
 def sum_two_values_with_return(first_value, second_value):
     my_sum = first_value + second_value
     return my_sum
@@ -44,5 +48,44 @@ my_result = sum_two_values_with_return(10, 5)
 print(my_result)
 print("")
 
-# 13:12
+# Función con parámetros de entrada/argumentos por clave
+print("Función con parámetros de entrada/argumentos por clave")
+
+def print_name(name, surname):
+    print(f"{name} {surname}")     # Lo que hace será interpolar y meter como texto.
+
+# print("Perucha", "Miguel")
+print_name(surname = "Perucha", name = "Miguel")    # Cómo decirle que el nombre es nombre y cuál el apellido para que los ponga correctos.
+print("")
+
+# Función con parámetros de entrada/argumentos por defecto
+print("Función con parámetros de entrada/argumentos por defecto")
+
+def print_name_with_default(name, surname, alias="Sin alias"):     # Alias con un valor por defecto, por si no nos dan ese dato.
+    print(f"{name} {surname} {alias}") 
+
+print_name_with_default("Miguel", "Perucha")
+print_name_with_default("Miguel", "Perucha", "Mapcdev")        # Si ponemos esto, imprime lo de dentro.
+print("")
+
+# Función con parámetros de entrada/argumentos arbitrarios
+print("Función con parámetros de entrada/argumentos arbitrarios")
+
+def print_texts(*texts):   # Así de simple, solo imprime un parámetro; si le ponemos delante * imprimirá todo lo que le pongamos.
+    for text in texts:
+        print(text)
+
+print_texts("Hola", "Python", "Miguel")
+print_texts("Miguel")
+print("")
+
+print("Haciendo que sean mayúsculas ")
+def print_upper_texts(*texts):   
+    print(type(texts))              # También podemos añadir que nos diga el tipo.
+    for text in texts:
+        print(text.upper())         # Añadimos upper a lo que nos tiene que mandar.
+
+print_upper_texts("Hola", "Python", "Miguel")
+print_upper_texts("Miguel")
+
 
