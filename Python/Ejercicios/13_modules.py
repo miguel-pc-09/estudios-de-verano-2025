cad6387528d82a3d4e43613f161ee95383e46bb4
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-10-09 18:16:19 +0200

    update

---
+++ Python/Ejercicios/13_modules.py
@@ -0,0 +1,49 @@
+# 1. Crea un módulo llamado "calculator" que contenga funciones para sumar, restar, multiplicar y dividir dos números. 
+# Importa este módulo en otro archivo y usa sus funciones.
+print("----- Ejercicio 1 ---------")
+import calculator
+calculator.sum_two_values(2, 3)
+calculator.res_two_values(7, 2)
+calculator.multi_two_values(5, 5)
+calculator.div_two_values(10, 2)
+print("")
+
+# 2. Crea un módulo llamado "converter" que tenga funciones para convertir temperaturas entre Celsius y Fahrenheit. Escribe un programa que importe este módulo y realice conversiones.
+print("----- Ejercicio 2 ---------")
+from converter import celsius_to_fahrenheit, fahrenheit_to_celsius
+print(celsius_to_fahrenheit(40))
+print(fahrenheit_to_celsius(40))
+print("")
+
+# 3. Crea un módulo que contenga una lista de nombres de estudiantes y una función que imprima todos los nombres. Importa este módulo en otro archivo y usa la función para mostrar la lista.
+print("----- Ejercicio 3 ---------")
+print("")
+
+# 4. Crea un módulo llamado "geometry" que tenga una función para calcular el área de un cí­rculo y un cuadrado. Usa este módulo en otro archivo para calcular áreas.
+print("----- Ejercicio 4 ---------")
+print("")
+
+# 5. Escribe un módulo que contenga una función que acepte cualquier nÃºmero de argumentos y devuelva su suma. Importa y usa la función en otro archivo.
+print("----- Ejercicio 5 ---------")
+print("")
+
+# 6. Crea un módulo que defina una clase llamada "Car" con propiedades como marca, modelo y año. Importa este módulo en otro archivo y crea una instancia de la clase "Car".
+print("----- Ejercicio 6 ---------")
+print("")
+
+# 7. Escribe un módulo que contenga funciones para leer y escribir en archivos de texto. Crea un programa que use estas funciones para escribir y leer datos.
+print("----- Ejercicio 7 ---------")
+print("")
+
+# 8. Crea un módulo llamado "statistics" que tenga funciones para calcular la media y la mediana de una lista de números. Usa este módulo para calcular estos valores en una lista dada.
+print("----- Ejercicio 8 ---------")
+print("")
+
+# 9. Crea un módulo que contenga una función para contar cuÃ¡ntas veces aparece una palabra en un texto. Escribe un programa que importe el módulo y lo use para contar palabras en una cadena.
+print("----- Ejercicio 9 ---------")
+print("")
+
+# 10 Crea un módulo llamado "dates" que contenga funciones para obtener la fecha actual y calcular la diferencia entre dos fechas. 
+# Usa este módulo en un programa para mostrar la fecha actual y la diferencia entre dos fechas especí­ficas.
+print("----- Ejercicio 10 ---------")
+print("")
---
+++ Python/Ejercicios/calculator.py
@@ -0,0 +1,15 @@
+# funciones para sumar, restar, multiplicar y dividir dos números
+
+def sum_two_values(oneValue, towValue):
+    
+    print("La suma es: ", oneValue + towValue)
+
+def res_two_values(oneValue, towValue):
+    
+    print("La resta es: ", oneValue - towValue)
+
+def multi_two_values(oneValue, towValue):
+    print("La multiplicación es: ",oneValue * towValue)
+
+def div_two_values(oneValue, towValue):
+    print("La división es: ",oneValue / towValue)
