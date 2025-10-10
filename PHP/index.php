d0f2f7015f71f9e10f72856aca6e0c2e687e8dce
miguel <m.peruchacastello@gmail.com>
2025-10-10 12:32:48 +0200

    update

--- PHP/index.php
+++ PHP/index.php
@@ -7,7 +7,19 @@
 # Variables
 $name = "Miguel";
 $isDev = true;
-$age = 39;
+$age = 37;
+// Metodo var_dump para ver el tipo de dato y el valor del dato en el momento que tiene esa variable
+// var_dump($name);
+// var_dump($isDev);
+// var_dump($age);
+// Tambien tenemos el gettype() que solo devuelve el tipo de dato
+// echo gettype($name);
+// echo gettype($isDev);
+// echo gettype($age);
+// Y otros como el is_string(), is_bool(), is_int() que devuelven true o false
+// echo is_string($name);
+// echo is_bool($isDev);
+// echo is_int($age);
 
 // Al ser devil, el mas es para sumar numeros, por mas que pongamos string lo va a tomar como int
 $newAge = 39 + '1';
@@ -18,7 +30,14 @@ $newAge2 = $age. '1'
 
 <h1>
     <?= $newAge2; ?>
-    <?= $name = "Miguel"; ?>
+    <br>
+    <?= 
+    // Tambien podemos concatenar con el punto de esta forma en caso de ser muy largo
+    "Hola " 
+    . $name 
+    . "Con una edad de " 
+    . $age 
+    .' 😁'; ?>
 </h1>
 
 <style>
