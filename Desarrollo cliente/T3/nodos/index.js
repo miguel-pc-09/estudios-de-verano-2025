555f0bd47cb6ac0528a3500948d7705da7bcd57c
miguel <m.peruchacastello@gmail.com>
2025-10-12 16:44:21 +0200

    update

---
+++ Desarrollo cliente/T3/nodos/index.js
@@ -0,0 +1,7 @@
+// Necesito el boton para que funcione
+let boton = document.querySelector("#btn-pulsar");
+boton.addEventListener("click", () => {
+  console.log("El boton a sido pulsado"); // para ver que funciona
+});
+// Es lo mismo que poner
+// boton.addEventListener("click", function name(params) {})
