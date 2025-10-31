235ab3e134f29ae9db07ea80dc65fef26902de6a
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-10-31 10:30:21 +0100

    update

---
+++ PHP/apuntes php/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // Comentario
+    /* Esto es otro comentario  */
+    # y este es otro comentario más
+    print"<u><b> Hola que tal </b></u>";
+    // echo: viene de linux
+    echo "<br> Segunda linea ";
+    echo "<br> Segunda linea "
+    ?>
+</body>
+
+</html>
